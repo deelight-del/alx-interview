a9681a02ab0bd08d18ef82e196388a532f45caa8
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-12 04:49:33 +0000

    task 0: pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""In this module, we construct the helper Function
+and actual function for deriving a pascal triangle"""
+
+
+def helpPascal(array):
+    """Helper Function for pascal triangle"""
+    newPascal = [1]
+    for i, v in enumerate(array[:-1]):
+        newPascal.append(
+            v + array[i + 1]
+        )
+    newPascal.append(1)
+    return newPascal
+
+
+def pascal_triangle(n):
+    """The main function that implements pascal triangle"""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    return pascal_triangle(n-1) + ([helpPascal(pascal_triangle(n-1)[-1])])
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,5 @@
+# PASCAL TRIANGLE
+
+In this technical interview, I try to
+construct a list of list that looks like the regular
+pascal triangle calculation.
