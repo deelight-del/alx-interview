0a4bf344e7842cf250f4a9829116feb642f993c9
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-16 17:40:03 +0000

    Add checker at beginning

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,6 +6,8 @@ boxes challenge"""
 def canUnlockAll(boxes):
     """Function to evaluate is given lockboxes
     can be fully opened"""
+    if not (isinstance(boxes, list) and len(boxes) > 0):
+        return False
     boxUnlocked = [False] * len(boxes)
     boxUnlocked[0] = True
     for i in range(len(boxes)):
