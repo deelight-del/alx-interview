c745e87d3406c424f0ab25d455f98ae0ee3307e6
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-16 17:36:55 +0000

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,6 @@ def canUnlockAll(boxes):
     """Function to evaluate is given lockboxes
     can be fully opened"""
     boxUnlocked = [False] * len(boxes)
-    print(boxUnlocked)
     boxUnlocked[0] = True
     for i in range(len(boxes)):
         for j in range(len(boxes)):
