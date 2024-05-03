cc7c2b2310111d31f133bb4795330209ef9b08b2
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-03 10:21:44 +0000

    xxx

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         """Define signal handler of CTRL+C"""
         print("File size: {}".format(total_size))
         [
-            print("{}: {}".format(k, resp_dict[k]))
+            print("{:d}: {}".format(k, resp_dict[k]))
             if resp_dict.get(k) > 0 else ""
             for k in sorted(list(resp_dict.keys()))
         ]
@@ -36,11 +36,11 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     for line in fileinput.input():
-        pattern1 = r"^(?:(?:\d){1,3}\.){3}(?:\d{1,3})\s\-\s"
+        pattern1 = r"^(?:(?:(?:\d){1,3}\.){3}(?:\d{1,3})|\S+)\s?\-\s?"
         pattern2 = r"\[\d{4}\-\d{2}\-\d{2}\s(?:\d{2}\:){2}\d\d\.\d{6}\]\s"
         pattern3 = r"\"GET\s\/projects\/260\sHTTP\/1\.1\"\s"
-        pattern4 = r"(200|301|400|401|403|404|405|500)\s"
-        pattern5 = r"([1-9][0-9]{0,2}[0-4]?)"
+        pattern4 = r"(200|301|400|401|403|404|405|500|\S*)?\s"
+        pattern5 = r"([1-9][0-9]{0,2}[0-4]{0,1})"
         pattern = pattern1 + pattern2 + pattern3 + pattern4 + pattern5
         match = re.search(pattern, line)
         if not match:
@@ -48,12 +48,16 @@ if __name__ == "__main__":
             # loop_count += 1
             # if loop_count % 10 == 0:
             #    signal_handler()
-        # if match:
+        #if match:
         #    print(line)
         # else:
         #    print("No match", line)
         resp_code, file_size = match.group(1, 2)
         total_size += int(file_size)
+        # Check if resp code is an int and if not, simulate an arbitray
+        # integer.
+        if not resp_code.isdigit():
+            resp_code = 50005  # Invalid.
         # Check if resp_code is valid among the available keys
         if resp_dict.get(int(resp_code)) is not None:
             resp_dict[int(resp_code)] += 1
@@ -61,7 +65,7 @@ if __name__ == "__main__":
         if loop_count % 10 == 0:
             print("File size: {}".format(total_size))
             [
-                print("{}: {}".format(k, resp_dict[k]))
+                print("{:d}: {}".format(k, resp_dict[k]))
                 if resp_dict.get(k) > 0 else ""
                 for k in sorted(resp_dict.keys())
             ]
