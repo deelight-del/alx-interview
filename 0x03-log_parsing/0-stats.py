3fb900db6e1fd66c8f4f24089afd0ecdf4d0a0e7
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-03 04:05:19 +0000

    task 0, up to date

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,6 +6,7 @@ from the input stream to a desired output"""
 import fileinput
 import re
 import sys
+import signal
 
 
 if __name__ == "__main__":
@@ -21,18 +22,41 @@ if __name__ == "__main__":
         500: 0
     }
     loop_count = 0
+    def signal_handler(_, __):
+        """Define signal handler of CTRL+C"""
+        print("File size: {}".format(total_size))
+        [
+            print("{}: {}".format(k, resp_dict[k]))
+            if resp_dict.get(k) > 0 else ""
+            for k in sorted(resp_dict.keys(), reverse=True)
+        ]
+
+    # Register the signal
+    signal.signal(signal.SIGINT, signal_handler)
+
     for line in fileinput.input():
         pattern1 = r"^(?:(?:\d){1,3}\.){3}(?:\d{1,3})\s\-\s"
         pattern2 = r"\[\d{4}\-\d{2}\-\d{2}\s(?:\d{2}\:){2}\d\d\.\d{6}\]\s"
         pattern3 = r"\"GET\s\/projects\/260\sHTTP\/1\.1\"\s"
-        pattern4 = r"(200|301|400|401|403|404|405|500)\s(1?[0-9]?[0-9]?[0-4]?){1}"
+        pattern4 = r"(200|301|400|401|403|404|405|500)\s([1-9][0-9]{0,2}|1024)"
         pattern = pattern1 + pattern2 + pattern3 + pattern4
         match = re.search(pattern, line)
+        if not match:
+            loop_count += 1
+            if loop_count % 10 == 0:
+                print("File size: {}".format(total_size))
+                [
+                    print("{}: {}".format(k, resp_dict[k]))
+                    if resp_dict.get(k) > 0 else ""
+                    for k in sorted(resp_dict.keys(), reverse=True)
+                ]
+            continue
         # if match:
         #    print(line)
         # else:
         #    print("No match", line)
         resp_code, file_size = match.group(1, 2)
+        # print("check", file_size, resp_code)
         total_size += int(file_size)
         if resp_dict.get(int(resp_code)) is not None:
             resp_dict[int(resp_code)] += 1
