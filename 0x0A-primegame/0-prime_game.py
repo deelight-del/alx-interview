0d6fa7fc8a0e8055f27576864fdf2ff1e7a02cdb
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-21 10:14:47 +0000

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -16,7 +16,6 @@ def sieve_of_n(n):
     playing_round = 0
     prime_number_idx = 1
     sieve = [i + 1 for i in range(n)]
-    print('sieve', sieve)
     while sieve[prime_number_idx] < (n / 2) + 1:
         prime_number = sieve[prime_number_idx]
         for number in sieve[prime_number_idx + 1:]:
